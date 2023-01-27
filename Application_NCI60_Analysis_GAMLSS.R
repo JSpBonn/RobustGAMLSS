4eaedf33a69143d11e691e53ba2cc961957709db
JSpBonn <64110672+JSpBonn@users.noreply.github.com>
2023-01-27 16:31:34 +0100

    Update Application_NCI60_Analysis_GAMLSS.R

--- Application_NCI60_Analysis_GAMLSS.R
+++ Application_NCI60_Analysis_GAMLSS.R
@@ -7,7 +7,7 @@
 
 # setwd("//folderapplication")
 source("Robust_gamboostLSS_Families.R") # loading some packages and the robust Gamma families object for GAMLSS gradient boosting
-load(data_set_application.RData) # the loaded data is called: "data_set" 
+load("data_set_application.RData") # the loaded data is called: "data_set" 
 
 
 number_cancer_cell_lines = 59
